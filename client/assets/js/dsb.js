0a3a7171c14e00315a217603e44680e4f6a88e80
rweekscodes <richard.weeks1946@gmail.com>
2024-01-05 17:55:56 +0100

    nc

--- client/assets/js/dsb.js
+++ client/assets/js/dsb.js
@@ -15,31 +15,10 @@ closeBtn.onclick = () => {
   menu.classList.remove("open-menu");
 };
 
-let accounts = [...document.querySelectorAll(".acc")];
-let allHistoryDOM = [...document.querySelectorAll(".hs")];
-
-function clearAllHistoryDOM() {
-  allHistoryDOM.forEach((h) => (h.innerHTML = ""));
-}
-
-accounts.forEach((account) => {
-  account.onclick = ({ target }) => {
-    let type = target.id;
-    let historyDOM = target.children[1].children[1];
-
-    if (target.classList.value.includes("open")) {
-      target.classList.remove("open");
-      return;
-    }
-
-    accounts.forEach((f) => f.classList.remove("open"));
-    target.classList.add("open");
-    openAccount(target, type, historyDOM);
-  };
-});
-
 // total costs
-let n = [];
+let n = [],
+  o = [];
+
 function totalCost(transDetails) {
   let b = [];
 
@@ -63,10 +42,70 @@ function totalCost(transDetails) {
 let pricesDOMs = [...document.querySelectorAll(".am")];
 
 let Details = Object.values(details);
+let accountTypes = Object.keys(details);
 
 Details = Details.map((M, i) => {
-  pricesDOMs[i].textContent = totalCost(M);
+  // console.log(totalCost(M));
+
+  o.push(totalCost(M));
+  // pricesDOMs[i].textContent = totalCost(M);
 });
+
+console.log(o);
+
+let accContainer = document.querySelector(".acc-container");
+let accountDOM = "";
+
+accountTypes.forEach((a, i) => {
+  accContainer.innerHTML += `
+<button id=${a} class="acc">
+		<div class="acc-head">
+			<h1>${a} **1123</h1>
+			<div
+				class="numbs">
+				<p class="cm title">Current</p>
+				<div class="dots"></div>
+				<p class="am current">${o[i]}</p>
+			</div>
+			<div class="numbs">
+				<p class="cm title">Available</p>
+				<div class="dots"></div>
+				<p class="am available">${o[i]}</p>
+			</div>
+
+		</div>
+		<div class="history">
+			<h1 class="hd">History</h1>
+			<div class="hs"></div>
+		</div>
+	</button>
+  `;
+});
+
+let accounts = [...document.querySelectorAll(".acc")];
+let allHistoryDOM = [...document.querySelectorAll(".hs")];
+
+function clearAllHistoryDOM() {
+  allHistoryDOM.forEach((h) => (h.innerHTML = ""));
+}
+
+accounts.forEach((account) => {
+  account.onclick = ({ target }) => {
+    let type = target.id;
+    let historyDOM = target.children[1].children[1];
+
+    if (target.classList.value.includes("open")) {
+      target.classList.remove("open");
+      return;
+    }
+
+    accounts.forEach((f) => f.classList.remove("open"));
+    target.classList.add("open");
+    openAccount(target, type, historyDOM);
+  };
+});
+
+// Accounts
 
 function openAccount(target, x, historyDOM) {
   const account = details[x];
@@ -79,7 +118,7 @@ function openAccount(target, x, historyDOM) {
     historyDOM.innerHTML += `
     <div class="t-sec">
         <div class="t-group"></div>
-	</div>`;
+	  </div>`;
   });
 
   transDetails.forEach((d, i) => {
